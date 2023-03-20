636b38e707129b7e225e2c4d32ab10665a0fc145
MaryKuzo <124557630+MaryKuzo@users.noreply.github.com>
2023-03-20 11:24:13 +0200

    1

---
+++ main.js
@@ -0,0 +1,7 @@
+import './src/modal-buy-now.js';
+import './src/slick.min.js';
+import './src/buy-now.js';
+import './src/mobile-menu.js';
+import './src/swiper.js';
+import './src/modal-review.js';
+import './src/modal-subscribe.js';
