c7292c3b49fcc76b0b74590936a2e84e4b459857
MaryKuzo <124557630+MaryKuzo@users.noreply.github.com>
2023-03-20 15:11:59 +0200

    1

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import './js/buy-now';
+import './js/slick.min.js';
 import './js/modal-review';
 import './js/modal-buy-now.js';
 import './js/mobile-menu.js';
@@ -6,4 +7,3 @@ import './js/mobile-menu.js';
 // import './js/modal-subscribe.js';
 // import './js/modal.js';
 // import './js/swiper.js';
-import '/js/slick.min.js';
