f865749cb644fd63220159f3fe0c568ca12c5476
MaryKuzo <124557630+MaryKuzo@users.noreply.github.com>
2023-03-21 11:00:03 +0200

    1

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,43 +1,50 @@
-const swiper = new Swiper('.swiper', {
-  breakpoints: {
+const { default: Swiper } = require("swiper");
+
+const sliders = document.querySelectorAll('.swiper');
+
+sliders.forEach((el) => {
+  let mySwiper = new Swiper(el, {
+    breakpoints: {
     // when window width is >= 320px
     320: {
       slidesPerView: 1,
-      slidesPerGroup:1,
+      slidesPerGroup: 1,
       spaceBetween: 16,
     },
     // when window width is >= 768px
     768: {
       slidesPerView: 2,
-      slidesPerGroup:2,
+      slidesPerGroup: 2,
       spaceBetween: 16
     },
     // when window width is >= 1200px
     1200: {
       slidesPerView: 3,
-      slidesPerGroup:3,
+      slidesPerGroup: 3,
       spaceBetween: 28
     }
   },
 
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
 
-  // If we need pagination
-  pagination: {
+    // If we need pagination
+    pagination: {
     el: '.swiper-pagination',
-    clickable: true,  
+    clickable: true,
   },
 
-  // Navigation arrows
-  navigation: {
+    // Navigation arrows
+    navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
-  // And if we need scrollbar
-  scrollbar: {
+    // And if we need scrollbar
+    scrollbar: {
     el: '.swiper-scrollbar',
   },
+});
+  
 });
