709d065f572cb706685177ec42ff553795b3800c
MaryKuzo <124557630+MaryKuzo@users.noreply.github.com>
2023-03-21 07:31:23 +0200

    1

---
+++ src/js/modal-buy-now.js
@@ -0,0 +1,36 @@
+  import Swiper from 'swiper';
+  // import Swiper styles
+  import 'swiper/css';
+
+
+const swiper2 = new Swiper('.buy-now-slider-container', {
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      slidesPerGroup:1,
+      spaceBetween: 16,
+    },
+  },
+
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.buy-now-swiper-pagination',
+    clickable: true,  
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
