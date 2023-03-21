2d352dd2988f5de4d7c4056cae92260db34f8492
MaryKuzo <124557630+MaryKuzo@users.noreply.github.com>
2023-03-21 15:22:44 +0200

    123

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,9 +1,7 @@
 
 
-const sliders = document.querySelectorAll('.swiper');
-
-sliders.forEach((el) => {
-  let mySwiper = new Swiper(el, {
+document.querySelectorAll('.swiper').forEach(el => {
+  const swiper = new Swiper(el, {
     breakpoints: {
     // when window width is >= 320px
     320: {
@@ -11,27 +9,13 @@ sliders.forEach((el) => {
       slidesPerGroup: 1,
       spaceBetween: 16,
     },
-    // when window width is >= 768px
-    768: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-      spaceBetween: 16
-    },
-    // when window width is >= 1200px
-    1200: {
-      slidesPerView: 3,
-      slidesPerGroup: 3,
-      spaceBetween: 28
-    }
-  },
 
-    // Optional parameters
+  },
     direction: 'horizontal',
     loop: true,
-
-    // If we need pagination
+    
     pagination: {
-    el: '.swiper-pagination',
+    el: el.querySelector('.swiper-pagination'),
     clickable: true,
   },
 
@@ -45,6 +29,6 @@ sliders.forEach((el) => {
     scrollbar: {
     el: '.swiper-scrollbar',
   },
-});
+})
   
 });
--- src/main.js
+++ src/main.js
@@ -1,15 +1,12 @@
 // import './js/slick.min';
-
+import './js/swiper.js'
 import './js/mobile-menu';
-import './js/buy-now';
+import './js/buy-now.js';
 import './js/modal-review';
 import './js/modal-subscribe';
 import './js/modal-thanks';
 import './js/modal-feedback';
 
-import './js/swiper';
-import './js/swiper-products';
 
 // import './js/how-its-made';
 
-import './js/modal-buy-now';
